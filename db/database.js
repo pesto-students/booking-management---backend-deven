d99888d41880c469f03cb9285f100bcc0a385e7b
Deven Sitapara <shriramsoft@gmail.com>
2023-04-07 20:20:38 +0530

    db connection log

--- db/database.js
+++ db/database.js
@@ -3,6 +3,13 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
 // mongoose.connect('mongodb://localhost:27017/pest-booking-management');
-mongoose.connect(
-  "mongodb+srv://sdeven:FWtQarp4fREW4dNH@cluster0.4c0ixve.mongodb.net/pesto-booking-management"
-);
+mongoose
+  .connect(
+    "mongodb+srv://sdeven:FWtQarp4fREW4dNH@cluster0.4c0ixve.mongodb.net/pesto-booking-management"
+  )
+  .then(() => {
+    console.log(`Database connected`);
+  })
+  .catch((err) => {
+    console.log(`Error connecting Database`, err);
+  });
